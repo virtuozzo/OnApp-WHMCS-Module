254fab61fb3d8de3e2dd536d0bc118817ec7598d
Andrew Yatskovets <jtsandrew@gmail.com>
2011-03-10 16:20:13 +0200

    Added functions ClientArea UnsuspendAccount SuspendAccount TerminateAccount and CreateAccount for onappbackupspace module

--- modules/servers/onappbackupspace/onappbackupspace.php
+++ modules/servers/onappbackupspace/onappbackupspace.php
@@ -1,7 +1,9 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
-require_once dirname(__FILE__).'/../onapp/onapp.php';
+require_once dirname(__FILE__).'/../onapp/lib.php';
+
+load_language();
 
 function onappbackupspace_ConfigOptions() {
     global $packageconfigoption, $_GET, $_POST, $_LANG;
@@ -10,16 +12,6 @@ function onappbackupspace_ConfigOptions() {
     $serviceid = addslashes($serviceid);
 
     $configarray = array();
-
-    $table_result = onapp_createTables();
-
-    if ( $table_result["error"] )
-        return array(
-            sprintf(
-                "<font color='red'><b>%s</b></font>",
-                $table_result["error"]
-            ) => array()
-        );
 
   ////////////////////////////
   // BEGIN Load Servers     //
@@ -165,6 +157,181 @@ $js_ConfigOptionsSub
     );
 
     return $configarray;
+}
+
+function onappbackupspace_CreateAccount($params) {
+    update_user_storagedisksize(
+        $params, 'Active'
+    );
+
+    return 'success';
+}
+
+function onappbackupspace_TerminateAccount( $params ) {
+    $serviceid = $params["serviceid"];
+
+    if ( canbedeleted($serviceid) ) {
+        update_user_storagedisksize(
+            $params, 'Terminate'
+        );
+
+        return 'success';
+    } else
+        return "You don't have enough disk space for backups";
+};
+
+function onappbackupspace_SuspendAccount($params) {
+    $serviceid = $params["serviceid"];
+
+    if ( canbedeleted($serviceid) ) {
+        update_user_storagedisksize(
+            $params, 'Pending'
+        );
+
+        return 'success';
+    } else
+        return "You don't have enough disk space for backups";
+}
+
+function onappbackupspace_UnsuspendAccount($params) {
+    update_user_storagedisksize(
+        $params, 'Active'
+    );
+
+    return 'success';
+}
+
+function canbedeleted($serviceid) {
+
+    $sql_select = "
+SELECT
+    tblhosting.id as id,
+    tblhosting.userid as userid,
+    tblonappclients.onapp_user_id,
+    tblproducts.configoption2 basespace,
+    CASE optiontype
+        WHEN 1 THEN optionssub.sortorder
+        WHEN 2 THEN optionssub.sortorder
+        WHEN 4 THEN options.qty * optionssub.sortorder
+        ELSE 0
+    END AS additionalspace,
+    tblservers.name as servername,
+
+    tblservers.id as serverid,
+    tblservers.ipaddress as serveripaddres,
+    tblservers.hostname  as serverhostname,
+    tblservers.username  as serverusername,
+    tblservers.password  as serverpassword
+FROM
+    tblhosting
+    LEFT JOIN tblproducts
+        ON tblproducts.id = packageid
+    LEFT JOIN tblservers
+        ON configoption1 = tblservers.id
+    LEFT JOIN tblonappclients ON
+        tblproducts.configoption1 = tblonappclients.server_id
+        AND tblhosting.userid = tblonappclients.client_id
+    LEFT JOIN tblhostingconfigoptions AS options ON
+        relid = tblhosting.id
+        AND options.configid = tblproducts.configoption3
+    LEFT JOIN tblproductconfigoptions
+        ON tblproductconfigoptions.id = options.configid
+    LEFT JOIN tblproductconfigoptionssub AS optionssub 
+        ON optionssub.configid = tblproductconfigoptions.id AND 
+        options.configid = tblproducts.configoption3 AND
+        options.optionid = optionssub.id
+WHERE
+    tblhosting.id = $serviceid";
+
+    $rows = full_query($sql_select);
+
+    $service = mysql_fetch_assoc($rows);
+
+    $server_id = $service["serverid"];
+    $user_id = $service["userid"];
+
+    $vms = new ONAPP_VirtualMachine();
+    $vms->auth(
+        $service["serveripaddres"] != "" ?
+            'http://' . $service["serveripaddres"] :
+            $service['serverhostname'],
+        $service['serverusername'],
+        decrypt($service['serverpassword'])
+    );
+
+    $backups_size = 0;
+
+    foreach ( $vms->getList($service['onapp_user_id']) as $vm ) {
+        $backups = new ONAPP_VirtualMachine_Backup();
+        $backups->_virtual_machine_id = $vm->_id;
+
+        $backups->auth(
+
+            $service["serveripaddres"] != "" ?
+                'http://' . $service["serveripaddres"] :
+                $service['serverhostname'],
+            $service['serverusername'],
+            decrypt($service['serverpassword'])
+        );
+
+        $backupslist = $backups->getList();
+
+        if ($backupslist)
+            foreach( $backups->getList() as $backup )
+                $backups_size += $backup->_backup_size;
+    };
+
+    $backups_size_gb = $backups_size / 1024 / 1024;
+
+
+    $sql_select_services = "
+SELECT
+    tblhosting.id as id,
+    tblproducts.configoption2 basespace,
+    CASE optiontype
+        WHEN 1 THEN optionssub.sortorder
+        WHEN 2 THEN optionssub.sortorder
+        WHEN 4 THEN options.qty * optionssub.sortorder
+        ELSE 0
+    END AS additionalspace
+FROM
+    tblhosting
+    LEFT JOIN tblproducts
+        ON tblproducts.id = packageid
+    LEFT JOIN tblservers
+        ON configoption1 = tblservers.id
+    LEFT JOIN tblonappclients ON
+        tblproducts.configoption1 = tblonappclients.server_id
+        AND tblhosting.userid = tblonappclients.client_id
+    LEFT JOIN tblhostingconfigoptions AS options ON
+        relid = tblhosting.id
+        AND options.configid = tblproducts.configoption3
+    LEFT JOIN tblproductconfigoptions
+        ON tblproductconfigoptions.id = options.configid
+    LEFT JOIN tblproductconfigoptionssub AS optionssub 
+        ON optionssub.configid = tblproductconfigoptions.id AND 
+        options.configid = tblproducts.configoption3 AND
+        options.optionid = optionssub.id
+WHERE
+    tblproducts.servertype = 'onappbackupspace'
+    AND tblhosting.domainstatus = 'Active'
+    AND tblhosting.userid = $user_id
+    AND tblservers.id = $server_id";
+
+    $services_rows = full_query($sql_select_services);
+    $availablespace = 0;
+
+    while ($backupservice =  mysql_fetch_assoc( $services_rows ) )
+        $availablespace += $backupservice['basespace'] + $backupservice['additionalspace'];
+
+
+   return ($availablespace - $backups_size_gb) >= ($service['basespace'] + $service['additionalspace']);
+}
+
+function onappbackupspace_ClientArea($params) {
+    global $_LANG;
+
+    return '<a href="onapp.php?page=storagedisksize">' . $_LANG["onappstoragedisksize"] . '</a>';
 }
 
 ?>
