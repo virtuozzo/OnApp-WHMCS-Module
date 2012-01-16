5c6f98c51b8b9f8a04ae785ce3113ddc2f3ccf0f
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2012-01-16 16:04:57 +0200

    Fixxed WHMCS Root path constant

--- modules/servers/onapp/cron_bandwidth.php
+++ modules/servers/onapp/cron_bandwidth.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 date_default_timezone_set('UTC');
 
-define("ROOT", "../../../");
+define("ROOT", realpath( '../../../' ). '/' );
 
 require_once ROOT . "dbconnect.php";
 require_once ROOT . "includes/functions.php";
